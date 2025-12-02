3121383287a5a60f3af203fcf5c6ccce66d2b5b0
dhanuprys <dhanuprys@gmail.com>
2025-12-02 18:19:46 +0800

    fix: support URL

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -69,7 +69,7 @@ export function Navigation() {
                             asChild
                         >
                             <a
-                                href="https://api.whatsapp.com/send?text=%5BAMERTA%20SUPPORT%5D%0A%0A*ISI%20DATA%20DIRI%20DI%20BAWAH*%20%0ANama%3A%20%0ANISN%3A%0ANIS%3A%0AKelas%3A%0A%0A%20_Tulis%20pesan%20anda%20disini_%0A%0ANB%3A%20Tambahkan%20screenshot%20(jika%20mengalami%20kendala)%20agar%20admin%20dapat%20menyelesaikan%20masalah%20lebih%20cepat"
+                                href="https://wa.me/6285646505614?text=%5BAMERTA%20SUPPORT%5D%0A%0A*ISI%20DATA%20DIRI%20DI%20BAWAH*%20%0ANama%3A%20%0ANISN%3A%0ANIS%3A%0AKelas%3A%0A%0A%20_Tulis%20pesan%20anda%20disini_%0A%0ANB%3A%20Tambahkan%20screenshot%20(jika%20mengalami%20kendala)%20agar%20admin%20dapat%20menyelesaikan%20masalah%20lebih%20cepat"
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
@@ -91,12 +91,12 @@ export function Navigation() {
                             asChild
                         >
                             <a
-                                href="https://api.whatsapp.com/send?text=%5BAMERTA%20SUPPORT%5D%0A%0A*ISI%20DATA%20DIRI%20DI%20BAWAH*%20%0ANama%3A%20%0ANISN%3A%0ANIS%3A%0AKelas%3A%0A%0A%20_Tulis%20pesan%20anda%20disini_%0A%0ANB%3A%20Tambahkan%20screenshot%20(jika%20mengalami%20kendala)%20agar%20admin%20dapat%20menyelesaikan%20masalah%20lebih%20cepat"
+                                href="https://wa.me/6285646505614?text=%5BAMERTA%20SUPPORT%5D%0A%0A*ISI%20DATA%20DIRI%20DI%20BAWAH*%20%0ANama%3A%20%0ANISN%3A%0ANIS%3A%0AKelas%3A%0A%0A%20_Tulis%20pesan%20anda%20disini_%0A%0ANB%3A%20Tambahkan%20screenshot%20(jika%20mengalami%20kendala)%20agar%20admin%20dapat%20menyelesaikan%20masalah%20lebih%20cepat"
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
                                 <Headset className="h-4 w-4" />
-                                <span className="font-medium">Contact</span>
+                                <span className="font-medium">Support</span>
                             </a>
                         </Button>
                         <DropdownMenu>
